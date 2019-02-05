108a642c2a492162fb97b80dda96c25f4294bb2e
Nolan Kovacik <noltron000@gmail.com>
2019-02-05 13:41:55 -0800

    🖌🎨 reformat file

--- server.go
+++ server.go
@@ -1,39 +1,39 @@
 package main
 
 import (
-    "net/http"
-    "encoding/json"
-    "io/ioutil"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
 
-    "github.com/labstack/echo"
-    "github.com/labstack/echo/middleware"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 type Job struct {
-    JobType string `json:"type"`
-    Company string `json:"company"`
-    Title string `json:"title"`
-    Location string `json:"location"`
-    Description string `json:"description"`
+	JobType     string `json:"type"`
+	Company     string `json:"company"`
+	Title       string `json:"title"`
+	Location    string `json:"location"`
+	Description string `json:"description"`
 }
 
 func main() {
-    // Echo instance
+	// Echo instance
 	e := echo.New()
 
 	// Logger middleware
 	e.Use(middleware.Logger())
 
-    // Calling GitHub jobs API
-    response, _ := http.Get("https://jobs.github.com/positions.json?description=python&full_time=true&location=sf")
-    // Get body of the response
-    data, _ := ioutil.ReadAll(response.Body)
-    // Instantiate a new job struct
-    var job []Job
-    // Unmarshal data into a pointer to the job struct
-    json.Unmarshal(data, &job)
-    // Create HTML string
-    jobPosting := "Company: <br>" + job[0].Company + "<br><br> Description: " + job[0].Description
+	// Calling GitHub jobs API
+	response, _ := http.Get("https://jobs.github.com/positions.json?description=python&full_time=true&location=sf")
+	// Get body of the response
+	data, _ := ioutil.ReadAll(response.Body)
+	// Instantiate a new job struct
+	var job []Job
+	// Unmarshal data into a pointer to the job struct
+	json.Unmarshal(data, &job)
+	// Create HTML string
+	jobPosting := "Company: <br>" + job[0].Company + "<br><br> Description: " + job[0].Description
 
 	// Route => handler
 	e.GET("/", func(c echo.Context) error {
