650351c151be96034d0f40aa936e7476ee38e9b7
Nolan Kovacik <noltron000@gmail.com>
2019-02-05 13:56:45 -0800

    🕴 more jobs!

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 
@@ -20,12 +21,12 @@ type Job struct {
 func main() {
 	// Echo instance
 	e := echo.New()
-
 	// Logger middleware
 	e.Use(middleware.Logger())
-
 	// Calling GitHub jobs API
 	response, _ := http.Get("https://jobs.github.com/positions.json?description=python&full_time=true&location=sf")
+	fmt.Println("HELLO WORLD")
+	fmt.Println(response)
 	// Get body of the response
 	data, _ := ioutil.ReadAll(response.Body)
 	// Instantiate a new job struct
@@ -33,13 +34,14 @@ func main() {
 	// Unmarshal data into a pointer to the job struct
 	json.Unmarshal(data, &job)
 	// Create HTML string
-	jobPosting := "Company: <br>" + job[0].Company + "<br><br> Description: " + job[0].Description
-
+	jobPosting := ""
+	for _, ChosenJob := range job {
+		jobPosting += "Company: <br>" + ChosenJob.Company + "<br><br> Description: " + ChosenJob.Description
+	}
 	// Route => handler
 	e.GET("/", func(c echo.Context) error {
 		return c.HTML(http.StatusOK, jobPosting)
 	})
-
 	// Start server
 	e.Logger.Fatal(e.Start(":3000"))
 }
